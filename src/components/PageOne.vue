a42b5316d25d906ecb404fffed94c4f332882152
fa1ryJack <7dan7iel7@gmail.com>
2024-12-07 02:37:17 +0300

    Added stuff to PageOne. ~half done

--- src/components/PageOne.vue
+++ src/components/PageOne.vue
@@ -8,6 +8,13 @@ export default {
         { text: "Title", value: "title" },
         { text: "Body", value: "body" },
       ],
+      drawer: false,
+      snackbar: false,
+      currentPost: {
+        id: 0,
+        title: "",
+        body: "",
+      },
     };
   },
   created: function () {
@@ -22,16 +29,61 @@ export default {
           this.posts[0];
         });
     },
+    row_class: function () {
+      return "row_class";
+    },
+    handleRowClick: function (event, row) {
+      this.currentPost = { ...row.item };
+      this.drawer = !this.drawer;
+    },
+    handleSubmit: function () {
+      this.posts = this.posts.map((post) =>
+        post.id === this.currentPost.id ? this.currentPost : post
+      );
+      this.snackbar = true;
+      this.drawer = !this.drawer;
+    },
   },
 };
 </script>
 
 <template>
-  <v-data-table
-    :items="posts"
-    class="mt-5"
-    :headers="headers"
-    :items-per-page="15"
-  >
-  </v-data-table>
+  <div>
+    <v-data-table
+      :items="posts"
+      class="mt-5"
+      height="79vh"
+      :headers="headers"
+      :items-per-page="10"
+      :item-class="row_class"
+      @click:row="handleRowClick"
+    >
+    </v-data-table>
+    <v-navigation-drawer width="35%" bottom app temporary v-model="drawer">
+      <v-form @submit.prevent="handleSubmit" autocomplete="off" class="ma-5">
+        <h2>Edit field:</h2>
+        <v-text-field
+          label="ID"
+          v-model="currentPost.id"
+          readonly
+        ></v-text-field>
+        <v-text-field label="Title" v-model="currentPost.title"></v-text-field>
+        <v-textarea label="Body" v-model="currentPost.body"></v-textarea>
+        <v-btn type="submit" block>Save</v-btn>
+      </v-form>
+    </v-navigation-drawer>
+
+    <v-snackbar v-model="snackbar" color="green" :timeout="2000">
+      Changes are saved!
+    </v-snackbar>
+  </div>
 </template>
+
+<style>
+.row_class {
+  cursor: pointer;
+}
+.v-snack__content {
+  text-align: center;
+}
+</style>
